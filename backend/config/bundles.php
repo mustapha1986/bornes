0c688df7c82de41cda73155eb4f0dd77781e5b30
Mustapha Ait idir <mustapha.aitidir@gmail.com>
2026-02-12 12:30:52 +0100

    Add Symfony bundle configuration

---
+++ backend/config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\\Bundle\\FrameworkBundle\\FrameworkBundle::class => ['all' => true],
+    Symfony\\Bundle\\MercureBundle\\MercureBundle::class => ['all' => true],
+    Doctrine\\Bundle\\MongoDBBundle\\DoctrineMongoDBBundle::class => ['all' => true],
+];
